b4c1f911bb1a2456f0815617acfefc95503983b4
Victor <victor@gbtsbuildingmaterials.com>
2025-06-14 10:19:21 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true
-};
-
-module.exports = nextConfig;
+export default defineNuxtConfig({
+  modules: [
+    '@nuxtjs/tailwindcss',
+  ],
+});
